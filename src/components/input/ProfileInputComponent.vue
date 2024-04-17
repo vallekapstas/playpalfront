7365078d3f7fd6fc5a4f33a725cc96e14a2036ae
fredb <fred.brosman06@gmail.com>
2024-04-17 10:54:50 +0300

    PLYP-25- WIP + Raini parandused

--- src/components/input/ProfileInputComponent.vue
+++ src/components/input/ProfileInputComponent.vue
@@ -2,21 +2,18 @@
   <div class="text-start bg-light border border-2 border-primary rounded-2 py-3 px-4 m-4 shadow-sm">
     <div class="row col-11 mx-auto needs-validation" novalidate>
       <div class="col-md-4" style="margin-top: 30px;">
-        <label for="validationCustom01" class="form-label">Eesnimi*</label>
-        <input type="text" class="form-control" id="validationCustom01" v-model="firstName" required>
-        <div class="valid-feedback" v-if="firstNameValid">
-          Sobib!
-        </div>
-        <div class = "invalid-feedback" v-else>
-        Palun sisesta eesnimi
+        <label for="firstName" class="form-label">Eesnimi*</label>
+        <input v-model="firstName" type="text" class="form-control" id="firstName" required>
+        <div v-if="!validFirstName" class="invalid-feedback" >
+          Palun sisesta eesnimi
         </div>
       </div>
 
       <div class="col-md-4" style="margin-top: 30px;">
-        <label for="validationCustom02" class="form-label">Perekonnanimi*</label>
-        <input type="text" class="form-control" id="validationCustom02" value="" required>
-        <div class="valid-feedback" >
-          Sobib!
+        <label for="lastName" class="form-label">Perekonnanimi*</label>
+        <input v-model="lastName" type="text" class="form-control" id="lastName" value="" required>
+        <div v-if="!validLastName" class="invalid-feedback" >
+          Palun sisesta perekonnanimi
         </div>
       </div>
 
@@ -44,7 +41,7 @@
       </div>
 
       <div class="col-md-4" style="margin-top: 30px;">
-        <label for="validationCustom01" class="form-label">Sünnikuupäev*</label>
+        <label for="firstName" class="form-label">Sünnikuupäev*</label>
         <input type="date" class="form-control">
         <div class="valid-feedback">
         </div>
@@ -53,14 +50,14 @@
       <div class="col-md-4"></div>
 
       <div class="col-md-4" style="margin-top: 30px;">
-        <label for="validationCustom01" class="form-label">Parool*</label>
+        <label for="firstName" class="form-label">Parool*</label>
         <input type="password" id="inputPassword5" class="form-control" aria-describedby="passwordHelpBlock">
         <div class="valid-feedback">
         </div>
       </div>
 
       <div class="col-md-4" style="margin-top: 30px;">
-        <label for="validationCustom01" class="form-label">Parool uuesti*</label>
+        <label for="firstName" class="form-label">Parool uuesti*</label>
         <input type="password" id="inputPassword5" class="form-control" aria-describedby="passwordHelpBlock">
         <div class="valid-feedback">
         </div>
@@ -74,7 +71,7 @@
 
 
       <div class="col-md-12 text-center" style="margin-top: 30px;">
-        <label for="validationCustom01" class="form-label">Sugu*</label>
+        <label for="firstName" class="form-label">Sugu*</label>
 
         <div class="row justify-content-center">
           <div class="col-md-4">
@@ -131,7 +128,9 @@ export default {
       errorMessage: '',
       selectedGender: null,
       firstName: '',
-      firstNameValid: false
+      lastName: '',
+      validFirstName: true,
+      validLastName: true,
 
     }
 
@@ -163,13 +162,16 @@ export default {
       }
     },
     submitForm() {
-      if (this.validateForm()){
-        this.goToEventView()
+      if (this.allFieldsWithCorrectInput()) {
+        // siis saadame backile sõnumi kasutaja registreerimisekt
       }
     },
-    validateForm() {
-      this.firstNameValid = !!this.firstName;
-      return this.firstNameValid;
+    allFieldsWithCorrectInput() {
+      this.validFirstName = this.firstName.length > 0
+      this.validLastName = this.lastName.length > 0
+
+
+      return this.validFirstName && this.validLastName
     },
     goToEventView() {
       router.push({name: 'indexRoute'})
@@ -179,49 +181,10 @@ export default {
     }
   },
 
-  mounted() {
-    // Add the script for disabling form submissions
-    const forms = document.querySelectorAll('.needs-validation');
-    Array.from(forms).forEach(form => {
-      form.addEventListener('submit', event => {
-        if (!form.checkValidity()) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-        form.classList.add('was-validated');
-      }, false);
-    });
-  }
 };
 
 
 </script>
-
-<style>
-.approval-tick {
-  color: green;
-}
-
-.rejection-tick {
-  color: red;
-}
-
-.input-group {
-  position: relative; /* Ensure relative positioning for absolute positioning */
-}
-
-.status-text {
-  position: absolute;
-  left: 10px; /* Adjust left position as needed */
-  top: 50%; /* Align vertically */
-  transform: translateY(-50%);
-}
-
-.rejection-tick,
-.approval-tick {
-  margin-right: 10px; /* Adjust margin as needed */
-}
-</style>
 
 
 
