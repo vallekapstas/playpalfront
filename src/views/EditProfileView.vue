16bd3a5c9fd697b3d0621b08577af2bb525e6609
Katrin <kat@hot.ee>
2024-04-24 13:07:59 +0300

    PLYP-34- KATRIN WIP

--- src/views/EditProfileView.vue
+++ src/views/EditProfileView.vue
@@ -1,11 +1,25 @@
 <template>
+<!--todo: kasutadta ProfileInputComponent-->
+
+<!--todo: loo nupp mis kääivitab PUT /user/{userId} teenuse-->
+
 
 </template>
 
 
 <script>
 export default {
-  name: 'EditProfileView'
+  name: 'EditProfileView',
+  userId: Number(sessionStorage.getItem('userId'))
+  // todo: too ära kasutaja andmed GET /user/userI
+  // todo: saad andmed kätte
+  // todo: profileInputComponentRef abil väärtusta ärd userProfileInfo objekt
+
+
+  // todo: loo meetod mis valideerib ära kõik väljad, (ProfileInputComponent-s vajalikud meetodid olemas)
+  // todo: saada PUT sõnum et salvestada muudatused
+
+
 }
 </script>
 
