da202ca936f18d7324a760d59b7faa1b1ff9cec1
Katrin <kat@hot.ee>
2024-04-26 10:28:17 +0300

    PLYP-33- KATRIN WIP

---
+++ src/components/modal/ProfilePreviewModal.vue
@@ -0,0 +1,24 @@
+<template>
+
+  <Modal ref="modalRef">
+
+    <ProfileView/>
+
+  </Modal>
+
+
+</template>
+
+
+<script>
+import Modal from "@/components/modal/Modal.vue";
+import ProfileView from "@/views/ProfileView.vue";
+
+export default {
+  name: "ProfilePreviewModal",
+  components: {ProfileView, Modal}
+}
+</script>
+
+
+
