da514354d30b1e75bba792ff818a684e31d12e25
Katrin <kat@hot.ee>
2024-04-26 15:58:24 +0300

    PLYP-33- KATRIN WIP

--- src/components/modal/ProfilePreviewModal.vue
+++ src/components/modal/ProfilePreviewModal.vue
@@ -6,7 +6,7 @@
       <div class="modal-dialog">
         <div class="modal-content">
           <div class="modal-header">
-            <h5 class="modal-title">Profiili vaade</h5>
+            <h5 class="modal-title">Kasutaja profiili eelvaade</h5>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
 
