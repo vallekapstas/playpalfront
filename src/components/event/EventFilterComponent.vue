22a72c5fd9fdffcec2943991d224585d9e6734d8
Valle Kapstas <valle.kapstas@gmail.com>
2024-04-26 10:39:25 +0300

    PLYP-39 WIP filter styling improved, adding filtering elements underway

--- src/components/event/EventFilterComponent.vue
+++ src/components/event/EventFilterComponent.vue
@@ -3,18 +3,24 @@
   <div class="container">
 
 
-      <div class="row event-filter text-start bg-transparent border border-2 border-secondary rounded-2 py-3 px-4 m-4 shadow-sm">
+    <div
+        class="event-filter text-start bg-transparent border border-2 border-secondary rounded-2 py-3 px-4 m-4 shadow-sm">
+
+      <div class="row mb-3 align-items-center d-flex mx-auto border-bottom border-secondary pb-3">
 
         <div class="col-6">
-          <div class="row">
-            <div class="col-3 align-content-center text-secondary text-end fw-bold">Staatus</div>
-            <div class="col-4">
+
+          <div class="row input-group gy-1">
+            <label class="col-lg-3 col-form-label text-secondary text-end fw-bold">Staatus</label>
+
+            <div class="col-lg-4">
               <select class="form-select border border-secondary-subtle">
                 <option value="1">on</option>
                 <option value="2">ei ole</option>
               </select>
             </div>
-            <div class="col-5">
+
+            <div class="col-lg-5">
               <select class="form-select border border-secondary-subtle">
                 <option value="1">aktiivne</option>
                 <option value="2">ootel</option>
@@ -22,29 +28,150 @@
               </select>
             </div>
           </div>
+
         </div>
 
         <div class="col-6">
+
           <div class="row">
-            <div class="col align-middle">
+            <div class="col-lg input-group my-2">
               <div class="form-check form-switch">
                 <input class="form-check-input" type="checkbox" role="switch" id="participant">
                 <label class="form-check-label fw-bold text-secondary" for="participant">Olen osaleja</label>
               </div>
             </div>
-            <div class="col">
+
+            <div class="col-lg input-group my-2">
               <div class="form-check form-switch">
                 <input class="form-check-input" type="checkbox" role="switch" id="eventhost">
                 <label class="form-check-label fw-bold text-secondary" for="eventhost">Olen korraldaja</label>
               </div>
             </div>
           </div>
+
         </div>
 
-      </div>
+      </div><!-- main row -->
 
+      <div class="row mb-3 align-items-center mx-auto border-bottom border-secondary pb-3">
 
-  </div>
+        <div class="col-6">
+
+          <div class="row input-group gy-1" aria-describedby="playersLimitDescription">
+            <div class="col-lg align-content-center text-secondary text-center">Osalejate piir</div>
+
+            <div class="col-lg">
+              <div class="row">
+                <div class="col">
+                  <input type="number" class="form-control" id="minPlayers" placeholder="0" step="1">
+                </div>
+              </div>
+            </div>
+
+            <div class="col-lg-1 align-content-center text-secondary text-center me-2">kuni</div>
+
+            <div class="col-lg">
+              <div class="row">
+                <div class="col">
+                  <input type="number" class="form-control" id="maxPlayers" placeholder="0" step="1">
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row form-text justify-content-center filter-helper-text" id="playersLimitDescription">Mängul osalejatele seatud osalejate arvu piir. 0 = ei otsi.<br>Sisesta üks või mõlemad.Mõlema numbri sisestamise puhul otsitakse vahemikku.</div>
+
+        </div>
+
+        <div class="col-6">
+
+          <div class="row input-group gy-1" aria-describedby="ageLimitDescription">
+            <div class="col-lg align-content-center text-secondary text-center">Vanusepiir</div>
+
+            <div class="col-lg">
+              <div class="row">
+                <div class="col">
+                  <input type="number" class="form-control" id="minAge" placeholder="0" step="1">
+                </div>
+              </div>
+            </div>
+
+            <div class="col-lg-1 align-content-center text-secondary text-center me-2">kuni</div>
+
+            <div class="col-lg">
+              <div class="row">
+                <div class="col">
+                  <input type="number" class="form-control" id="maxAge" placeholder="0" step="1">
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row form-text justify-content-center filter-helper-text" id="ageLimitDescription">Mängul osalejatele seatud vanusepiir. 0 = ei otsi. Sisesta üks või mõlemad.<br>Mõlema numbri sisestamise puhul otsitakse vahemikku.</div>
+
+        </div>
+
+      </div><!-- main row -->
+
+      <div class="row mb-3 align-items-center mx-auto">
+
+        <div class="col-6">
+
+          <div class="row input-group gy-1" aria-describedby="feeDescription">
+            <div class="col-lg align-content-center text-secondary text-center">Osalustasu</div>
+
+            <div class="col-lg">
+              <div class="row">
+                <div class="col">
+                  <input type="number" class="form-control" id="minFee" placeholder="0" step="1">
+                </div>
+              </div>
+            </div>
+
+            <div class="col-lg-1 align-content-center text-secondary text-center me-2">kuni</div>
+
+            <div class="col-lg">
+              <div class="row">
+                <div class="col">
+                  <input type="number" class="form-control" id="maxFee" placeholder="0" step="1">
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row form-text justify-content-center filter-helper-text" id="feeDescription">Mängu osavõtutasu. 0 = ei otsi. Sisesta üks või mõlemad.<br>Mõlema numbri sisestamise puhul otsitakse vahemikku.</div>
+
+        </div>
+
+        <div class="col-6">
+
+          <div class="row input-group gy-1" aria-describedby="playersJoinedDescription">
+            <div class="col-lg align-content-center text-secondary text-center">Mängijate arv</div>
+
+            <div class="col-lg">
+              <div class="row align-items-center">
+                <div class="col">
+                  <input type="number" class="form-control" id="minPlayersJoined" placeholder="0" step="1">
+                </div>
+              </div>
+            </div>
+
+            <div class="col-lg-1 align-content-center text-secondary text-center me-2">kuni</div>
+
+            <div class="col-lg">
+              <div class="row">
+                <div class="col">
+                  <input type="number" class="form-control" id="maxPlayersJoined" placeholder="0" step="1">
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row form-text justify-content-center filter-helper-text" id="playersJoinedDescription">Mänguga liitunud mängijate arv. 0 = ei otsi. Sisesta üks või mõlemad.<br>Mõlema numbri sisestamise puhul otsitakse vahemikku.</div>
+
+        </div>
+
+      </div><!-- main row -->
+
+    </div><!-- master styling wrapper -->
+
+  </div><!-- container -->
 
 </template>
 
